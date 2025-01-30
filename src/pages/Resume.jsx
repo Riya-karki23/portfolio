98e152dae2edaca903484f68ebd8c6a84c2755ab
Riya Karki <riyakarki2308@gmail.com>
2025-01-30 14:08:38 +0530

    changed image path

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -10,7 +10,7 @@ function Resume() {
     <div className="bg-[#1e1e1f] md:w-[60%] rounded-4xl border-2 border-[#353536]">
     <Header name='Resume'/>
     <div className="pl-16 md:mt-14 absolute right-7 ">
-      <a  className="bg-[#585858] md:px-4 md:py-2 px-2 py-1 rounded-lg" href="/assets/Resume(Riya karki).jpg " download="Resume(Riya karki).jpg">Download Resume
+      <a  className="bg-[#585858] md:px-4 md:py-2 px-2 py-1 rounded-lg" href="/assets/Resume(Riya karki).jpg" download="Resume(Riya karki).jpg">Download Resume
       </a>
       </div>
     <div className=" md:px-16 py-10 ">
