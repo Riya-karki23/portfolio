5c9772ac1b66de9536619f623611db559f8935cd
Riya Karki <riyakarki2308@gmail.com>
2025-04-16 12:03:49 +0530

    changed ui

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,9 +14,9 @@ function Card() {
     </div>
     <div className="p-5">
         <a href="#">
-            <h5 className="mb-2 md:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{elem.name}</h5>
+            <h5 className="mb-2 md:text-2xl font-bold tracking-tight text-white">{elem.name}</h5>
         </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{elem.description}</p>
+        <p className="mb-3 font-normal text-gray-400">{elem.description}</p>
         <div className="flex justify-between ">
         <Link to={elem.link} className="inline-flex  items-center px-1 md:px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             View Project
